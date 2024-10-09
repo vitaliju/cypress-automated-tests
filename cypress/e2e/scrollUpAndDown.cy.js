303388d0a304300562f937701a5ae47097d70e5c
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-10-09 16:50:36 +0300

    scrollUp

--- cypress/e2e/scrollUpAndDown.cy.js
+++ cypress/e2e/scrollUpAndDown.cy.js
@@ -8,6 +8,11 @@ describe('Scroll up using "Arrow" button and scroll down functionality', () => {
         cy.scrollTo('bottom');
         cy.contains('h2', 'Subscription').should('be.visible');
 
-
+        // Scroll up using 'Arrow' button and verify 
+        // that 'Full-Fledged practice website for Automation Engineers' is displayed
+        cy.get('#scrollUp').click();
+        cy.get('.carousel-inner')
+            .should('contain', 'Full-Fledged practice website for Automation Engineers')
+            .and('be.visible');
     });
 });
