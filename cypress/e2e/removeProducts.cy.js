0e5904e74aeff9b5dcd5bfbbd08d6ebead9bd64c
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-10-10 10:49:51 +0300

    remove products

---
+++ cypress/e2e/removeProducts.cy.js
@@ -0,0 +1,15 @@
+describe('Remove product from the cart', () => {
+    it('should allow to remove product from the cart', () => {
+        const productId = '2';
+        // Verify that home page is visible
+        cy.visitHome();
+
+        // Add product to cart and navigate to view_cart page
+        cy.get(`a[data-product-id="${productId}"]`).first().contains('Add to cart').trigger('mouseover');
+        cy.get(`a[data-product-id="${productId}"]`).first().contains('Add to cart').click();
+        cy.get('.btn.btn-success.close-modal').should('be.visible').click();
+        cy.get('a[href="/view_cart"]').first().should('contain.text', 'Cart').click();
+
+
+    });
+});
