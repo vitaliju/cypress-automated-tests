a0d786fc0090e6d0f005fae3ad3d93fd220ef9e1
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-10-10 12:18:39 +0300

    update

--- cypress/e2e/verifyProducts.cy.js
+++ cypress/e2e/verifyProducts.cy.js
@@ -13,6 +13,14 @@ describe('"ALL PRODUCTS" and product detail pages load successfully', () => {
         cy.get('.col-sm-4').should('be.visible');
         cy.get('a[href="/product_details/1"]').click();
 
-
+        // Verify that all product details are visible
+        cy.url().should('eq', 'https://www.automationexercise.com/product_details/1');
+        cy.get('.product-details').should('be.visible');
+        cy.contains('h2', 'Blue Top').should('be.visible');
+        cy.get('p').should('contain', 'Category:').and('be.visible');
+        cy.get('span span').should('contain', 'Rs. 500').and('be.visible');
+        cy.get('p').should('contain', 'Availability:').and('be.visible');
+        cy.get('p').should('contain', 'Condition:').and('be.visible');
+        cy.get('p').should('contain', 'Brand:').and('be.visible');
     });
 });
